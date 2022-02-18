17009eb71e4cba845706d3a20ecab15068b89a21
Akshay Kumar Barya <akshaykumarbarya@BGVMC463.local>
2022-02-18 19:35:32 +0530

    Completed the boiler plate of project

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      App
+      <h1 className="chu">chu</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/common/GameComponent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const GameComponent = (props: Props) => {
+  return <div>GameComponent</div>;
+};
+
+export default GameComponent;
